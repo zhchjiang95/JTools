1bd3bdb80a2b8f8703b1543ea3d0d4a1264a52cb
张长江 <1819211395@qq.com>
2020-10-09 10:58:31 +0800

    update vite/vue

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  base: './',
+  proxy: {
+    // string shorthand
+    '/foo': 'http://localhost:4567/foo',
+    // with options
+    '/api': {
+      target: 'http://jsonplaceholder.typicode.com',
+      changeOrigin: true,
+      rewrite: path => path.replace(/^\/api/, '')
+    }
+  }
+}
